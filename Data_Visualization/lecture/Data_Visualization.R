71dafa0bb916de3de3b95b7c0b4dd514b0dd666f
muschellij2 <muschellij2@gmail.com>
2017-01-11 20:07:14 -0500

    fixing data viz

--- Data_Visualization/lecture/Data_Visualization.R
+++ Data_Visualization/lecture/Data_Visualization.R
@@ -7,7 +7,7 @@ opts_chunk$set(echo = TRUE,
                fig.width = 7, 
                comment = "")
 
-## ----seed, comment="",echo=FALSE,prompt=TRUE-----------------------------
+## ----seed, comment="",echo=FALSE-----------------------------------------
 set.seed(3) 
 
 ## ----plotEx,  fig.align='center',cache=FALSE-----------------------------
@@ -20,7 +20,7 @@ death[1:2, 1:5]
 colnames(death)[1] = "country"
 death[1:2, 1:5]
 
-## ----plot1, comment="",prompt=TRUE,  fig.align='center',cache = FALSE----
+## ----plot1, comment="",  fig.align='center',cache = FALSE----------------
 library(dplyr)
 sweden = death %>% 
   filter(country == "Sweden") %>% 
@@ -28,7 +28,7 @@ sweden = death %>%
 year = as.numeric(colnames(sweden))
 plot(as.numeric(sweden) ~ year)
 
-## ----plotEx2, comment="",prompt=TRUE,  fig.align='center',cache = FALSE----
+## ----plotEx2, comment="",  fig.align='center',cache = FALSE--------------
 plot(as.numeric(sweden) ~ year,
       ylab = "# of deaths per family", main = "Sweden", type = "l")
 
@@ -58,7 +58,7 @@ plot(deaths ~ year, data = swede_long)
 library(ggplot2)
 qplot(x = year, y = deaths, data = swede_long)
 
-## ----generic_gg, comment="", prompt=TRUE, fig.align='center', cache=FALSE----
+## ----generic_gg, comment="",  fig.align='center', cache=FALSE------------
 g = ggplot(data = swede_long, aes(x = year, y = deaths))
 
 ## ----gprint_point--------------------------------------------------------
@@ -77,8 +77,7 @@ g + geom_line() + geom_smooth()
 sub = long %>% filter(country %in% 
                         c("United States", "United Kingdom", "Sweden",
                           "Afghanistan", "Rwanda"))
-g = ggplot(sub, aes(x = year, y = deaths, 
-  colour = country))
+g = ggplot(sub, aes(x = year, y = deaths, colour = country))
 g + geom_line()
 
 ## ----geom_noguide--------------------------------------------------------
@@ -112,11 +111,9 @@ sub %>% ggplot(aes(x = year, y = deaths)) +
 ##   facet_wrap(~ country + x2 + ... )
 
 ## ----labs, eval = TRUE---------------------------------------------------
-q = qplot(x = year, y = deaths, 
-          colour = country, data = sub,
+q = qplot(x = year, y = deaths, colour = country, data = sub,
           geom = "line") + 
-  xlab("Year of Collection") +
-  ylab("Deaths /100,000") +
+  xlab("Year of Collection") + ylab("Deaths /100,000") +
   ggtitle("Mortality of Children over the years",
           subtitle = "not great") 
 q
@@ -125,14 +122,13 @@ q
 q + theme_bw()
 
 ## ----theme, eval = TRUE--------------------------------------------------
-q + theme(text = element_text(size = 12),
-          title = element_text(size = 20))
+q + theme(text = element_text(size = 12), title = element_text(size = 20))
 
 ## ----theme2, eval = TRUE-------------------------------------------------
 q = q + theme(axis.text = element_text(size = 14),
           title = element_text(size = 20),
           axis.title = element_text(size = 16),
-          legend.position = c(0.75, 0.5)) + 
+          legend.position = c(0.9, 0.8)) + 
   guides(colour = guide_legend(title = "Country"))
 q
 
@@ -143,48 +139,46 @@ transparent_legend =  theme(legend.background = element_rect(
                             color = "transparent") )
 q + transparent_legend
 
-## ----hist_death, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----hist_death, comment="", fig.align='center', cache=FALSE-------------
 hist(sub$deaths, breaks = 200)
 
-## ----ghist, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
-qplot(x = deaths, 
-      fill = factor(country),
-      data = sub, 
-      geom = c("histogram"))
+## ----ghist, comment="", fig.align='center', cache = FALSE----------------
+qplot(x = deaths, fill = factor(country),
+      data = sub, geom = c("histogram"))
 
-## ----ghist_alpha, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----ghist_alpha, comment="", fig.align='center', cache = FALSE----------
 qplot(x = deaths, fill = country, data = sub, 
-      geom = c("histogram"), alpha=I(.7))
+      geom = c("histogram"), alpha=.7)
 
-## ----gdens, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----gdens, comment="", fig.align='center', cache = FALSE----------------
 qplot(x= deaths, fill = country, data = sub, 
-      geom = c("density"), alpha=I(.7))
+      geom = c("density"), alpha= .7)
 
-## ----gdens_alpha, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----gdens_alpha, comment="", fig.align='center', cache = FALSE----------
 qplot(x = deaths, colour = country, data = sub, 
-      geom = c("density"), alpha=I(.7))
+      geom = c("density"), alpha= .7)
 
-## ----gdens_line_alpha_death, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----gdens_line_alpha_death, comment="", fig.align='center', cache = FALSE----
 ggplot(aes(x = deaths, colour = country), data = sub) + 
   geom_line(stat = "density")
 
-## ----geom_line_qplot_nocol, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----geom_line_qplot_nocol, comment="", fig.align='center', cache=FALSE----
 qplot(x = year, y = deaths, colour = country, 
     data = long, geom = "line") + guides(colour = FALSE)
 
 ## ----geom_tile_qtile-----------------------------------------------------
-qtile = qplot(x = year, y = country, fill = deaths, 
-    data = sub, geom = "tile") + 
-  xlim(1990, 2005) + guides(colour = FALSE)
-qtile 
+qtile = qplot(x = year, y = country, fill = deaths, data = sub, 
+              geom = "tile") + xlim(1990, 2005) + guides(colour = FALSE)
+
+## ----geom_tile_qtile_show, echo = FALSE----------------------------------
+qtile
 
 ## ----geom_tile_rescale---------------------------------------------------
 qtile + scale_fill_gradient( low = "blue", high = "red")
 
 ## ----geom_tile_cut-------------------------------------------------------
 sub$cat = cut(sub$deaths, breaks = c(0, 1, 2, max(sub$deaths)))
-qplot(x = year, y = country, fill = cat, 
-    data = sub, geom = "tile") + 
+qplot(x = year, y = country, fill = cat, data = sub, geom = "tile") + 
   guides(colour = FALSE)
 
 ## ----barplot2, fig.align='center', cache = FALSE-------------------------
@@ -235,7 +229,7 @@ barplot(counts, main="Car Distribution by Age and Bad Buy Status",
   xlab="Vehicle Age", col=c("darkblue","red"),
     legend = rownames(counts), beside=TRUE)
 
-## ----boxplots, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----boxplots, comment="", fig.align='center', cache=FALSE---------------
 boxplot(weight ~ Diet, data=ChickWeight, outline=FALSE)
 points(ChickWeight$weight ~ jitter(as.numeric(ChickWeight$Diet),0.5))
 
@@ -274,7 +268,7 @@ legend("topleft", paste("Diet",levels(ChickWeight$Diet)),
         col = 1:length(levels(ChickWeight$Diet)),
        lwd = 3, ncol = 2)
 
-## ----circ, comment="",prompt=TRUE, fig.align='center', cache=FALSE-------
+## ----circ, comment="", fig.align='center', cache=FALSE-------------------
 circ = read_csv("http://www.aejaffe.com/winterR_2017/data/Charm_City_Circulator_Ridership.csv")
 palette(brewer.pal(7,"Dark2"))
 dd = factor(circ$day)
@@ -282,56 +276,56 @@ plot(orangeAverage ~ greenAverage, data=circ,
      pch=19, col = as.numeric(dd))
 legend("bottomright", levels(dd), col=1:length(dd), pch = 19)
 
-## ----circ2, comment="",prompt=TRUE, fig.align='center', cache=FALSE------
+## ----circ2, comment="", fig.align='center', cache=FALSE------------------
 dd = factor(circ$day, levels=c("Monday","Tuesday","Wednesday",
             "Thursday","Friday","Saturday","Sunday"))
 plot(orangeAverage ~ greenAverage, data=circ,
      pch=19, col = as.numeric(dd))
 legend("bottomright", levels(dd), col=1:length(dd), pch = 19)
 
-## ----hist, comment="",prompt=TRUE, fig.align='center', cache=FALSE-------
+## ----hist, comment="", fig.align='center', cache=FALSE-------------------
 hist(ChickWeight$weight, breaks = 20)
 
-## ----ghist_chick, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----ghist_chick, comment="", fig.align='center', cache = FALSE----------
 qplot(x = weight, 
       fill = factor(Diet),
       data = ChickWeight, 
       geom = c("histogram"))
 
-## ----ghist_chick_alpha, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----ghist_chick_alpha, comment="", fig.align='center', cache = FALSE----
 qplot(x = weight, fill = Diet, data = ChickWeight, 
-      geom = c("histogram"), alpha=I(.7))
+      geom = c("histogram"), alpha=.7)
 
-## ----gdens_chick, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----gdens_chick, comment="", fig.align='center', cache = FALSE----------
 qplot(x= weight, fill = Diet, data = ChickWeight, 
-      geom = c("density"), alpha=I(.7))
+      geom = c("density"), alpha= .7)
 
-## ----gdens_chick_alpha, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----gdens_chick_alpha, comment="", fig.align='center', cache = FALSE----
 qplot(x= weight, colour = Diet, data = ChickWeight, 
-      geom = c("density"), alpha=I(.7))
+      geom = c("density"), alpha=.7)
 
-## ----gdens_alpha_gg, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----gdens_alpha_gg, comment="", fig.align='center', cache = FALSE-------
 ggplot(aes(x= weight, colour = Diet), 
-  data = ChickWeight) + geom_density(alpha=I(.7))
+  data = ChickWeight) + geom_density(alpha=.7)
 
-## ----gdens_line_alpha, comment="",prompt=TRUE, fig.align='center', cache = FALSE----
+## ----gdens_line_alpha, comment="", fig.align='center', cache = FALSE-----
 ggplot(aes(x = weight, colour = Diet), data = ChickWeight) + 
   geom_line(stat = "density")
 
-## ----spaghetti, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----spaghetti, comment="", fig.align='center', cache=FALSE--------------
 qplot(x=Time, y=weight, colour = Chick, 
       data = ChickWeight, geom = "line")
 
-## ----fac_spag, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----fac_spag, comment="", fig.align='center', cache=FALSE---------------
 qplot(x = Time, y = weight, colour = Chick, 
       facets = ~Diet, data = ChickWeight, geom = "line")
 
-## ----fac_spag_noleg, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----fac_spag_noleg, comment="", fig.align='center', cache=FALSE---------
 qplot(x=Time, y=weight, colour = Chick, 
       facets = ~ Diet,  data = ChickWeight, 
         geom = "line") + guides(colour=FALSE)
 
-## ----fac_spag2, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----fac_spag2, comment="", fig.align='center', cache=FALSE--------------
 ggplot(aes(x = Time, y = weight, colour = Chick), 
     data = ChickWeight) + geom_line() + 
     facet_wrap(facets = ~Diet) + guides(colour = FALSE)
@@ -349,7 +343,7 @@ library(dplyr)
 long$year = long$year %>% str_replace("^X", "") %>% as.numeric
 long = long %>% filter(!is.na(deaths))
 
-## ----geom_line_qplot, comment="",prompt=TRUE, fig.align='center', cache=FALSE----
+## ----geom_line_qplot, comment="", fig.align='center', cache=FALSE--------
 qplot(x = year, y = deaths, colour = state, 
     data = long, geom = "line") + guides(colour = FALSE)
 
