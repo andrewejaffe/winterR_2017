fa7ecb55e901acdb50b942cbbcf085fd4b34b108
andrewejaffe <andrewejaffe@gmail.com>
2017-01-12 18:30:28 -0500

    add video

--- Manipulating_Data_in_R/lab/Data_Manipulation_Lab_Key.R
+++ Manipulating_Data_in_R/lab/Data_Manipulation_Lab_Key.R
@@ -25,24 +25,30 @@ length(unique(crash$Year))
 
 ## 4. Join data to retain only complete data, 
 ##	e.g. those observations with road lengths and districts
-dat = right_join(crash, road)
+dat = inner_join(crash, road,by="Road")
+dat = merge(crash, road, by="Road") %>% as.tbl
 
 ## 5. Which district had the largest volume of traffic, 
 ##		per mile/unit length over the provided years?
 dat$VolPerMile = dat$Volume/dat$Length
 dat = dat %>% mutate(VolPerMile = Volume/Length)
+
 dat %>% group_by(District) %>% 
-	summarize(Total = sum(VolPerMile,na.rm=TRUE))
+	summarize(Total = sum(VolPerMile))
+
 # all in one shot
 dat %>% mutate(VolPerMile = Volume/Length) %>% 
-	group_by(District) %>% 
-	summarize(Total = sum(VolPerMile,na.rm=TRUE))
+	group_by(District) %>% summarize(Total = sum(VolPerMile))
+
 
 ## 6. Which district had the largest number of crashes, 
 ##		accounting for road length and volume 
 ## 		over the provided years?
 
 dat %>% mutate(CrashAdj = N_Crashes/(Volume/1000)/Length) %>% 
-	group_by(District) %>% 
-	summarize(Total = sum(CrashAdj,na.rm=TRUE))
+	group_by(District) %>% summarize(Total = sum(CrashAdj))
 ## per thousand cars and mile
+
+
+dat %>% group_by(District) %>%
+    summarize(Total = sum(N_Crashes))
